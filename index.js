43d7a42a7d40a50dd2f12430223b75fb0e6c2162
GuiGui <jeanguillaumethomas3@gmail.com>
2022-02-11 08:05:46 -0800

    tried adding smooth scroll with Js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Hamburger Menu 
+
 const navButton = document.querySelector(".mobile-nav")
 
 const navLinks = document.querySelector(".nav-links-mobile");
 
- openMenu=()=>{
+ function openMenu(){
      function fadeOut(){
         navLinks.style.opacity="0";
         navLinks.style.display="none";
@@ -21,16 +23,17 @@ const navLinks = document.querySelector(".nav-links-mobile");
 
 navButton.onclick=openMenu;
 
-
 const navBar=document.querySelector("nav");
 
 const title=document.querySelector(".title")
 
 
 const sectionOneOptions = {
-    rootMargin: "-200px 0px 0px 0px"
+    rootMargin : "-200px 0px 0px 0px"
 };
 
+
+// Nav Bar Animation
 
 const sectionOneObserver = new IntersectionObserver (function(entries,sectionOneObserver){
     entries.forEach(entry =>{
@@ -45,3 +48,47 @@ const sectionOneObserver = new IntersectionObserver (function(entries,sectionOne
 },sectionOneOptions);
 
 sectionOneObserver.observe(title);
+
+
+// For Fading in effect 
+
+const faders = document.querySelectorAll(".faders")
+
+const Options= {
+    threshold:.6,
+}
+
+const sectionTwoObserver = new IntersectionObserver(function(entries,sectionTwoObserver){
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            entry.target.classList.add("appear")
+            sectionTwoObserver.unobserve(entry.target);
+        }
+    })
+},Options);
+
+faders.forEach(fader=>{
+    sectionTwoObserver.observe(fader);
+});
+
+
+const homeButton = document.querySelector(".home-btn")
+const aboutButton = document.querySelector(".about-btn")
+const featuresButton = document.querySelector(".features-btn")
+const discordButton = document.querySelector(".discord-btn")
+
+const featuresView = document.querySelector("#features")
+
+
+const navbtn= document.querySelectorAll(".nav-links a")
+
+function smoothScroll(){
+    featuresView.scrollIntoView({
+        behavior:"smooth",
+        block:"start"
+    })
+}
+console.log(featuresButton,featuresView)
+featuresButton.addEventListener("click",smoothScroll);
